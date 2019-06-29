7ce162145ee32cb92e3bb97983fa87447f401f34
Diego Cruz <abeja.dact_@hotmail.Com>
2019-06-29 09:55:15 -0500

    fs write and read

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
  * *************
  * 
  */
+
+const fs= require("fs")
+const ops= require("./src/fileops")
+
+let incValue
+
+fs.readFile("./resources/number.txt","utf8",(err,text)  => {
+  if(err) throw err
+  //console.log(text) 
+  const numbers=text.split("\n").map(n => Number(n))
+  console.log(numbers)
+  //console.log(ops.incrementValues(numbers))
+  incValue=ops.incrementValues(numbers)
+  fs.writeFile("./resources/numbernew.txt",incValue.join("\n"),(err,result)=> {
+    if(err) throw err
+})
+
+})
+
+
+
