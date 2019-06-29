596499b04250b572e8b9b6179c1bda605d3af459
Diego Cruz <abeja.dact_@hotmail.Com>
2019-06-29 08:25:16 -0500

    callbacks

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
  * 
  * *************
  * Comience aqui
- * *************
+ **************
  * 
  */
+
+const call = require("./src/call")
+/*
+call.withCallback("Diego Cruz",call.sync)
+*/
+call.withPromise("Diego Cruz")
+    .then(name => { console.log(name)})
+
