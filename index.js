6e87eaecb5c54d7a504e374374e9c564de7e772d
Diego Cruz <abeja.dact_@hotmail.Com>
2019-06-29 08:46:49 -0500

    Async Await

--- index.js
+++ index.js
@@ -5,3 +5,18 @@
  * *************
  * 
  */
+
+ const call = require("./src/call")
+
+ //call.greet("Diego Cruz")
+
+ /*call.withPromise("Diego","Cruz")
+ .then(n=>console.log(n))*/
+
+ async function calwithPromise(){ 
+     const fullName = await call.withPromise("Diego","Cruz")
+     console.log(fullName)
+    }
+
+calwithPromise()
+ 
